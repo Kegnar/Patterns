995ace872c4475f3cc3cf8e5f7845e6b2e007d7b
Kegnar <mitriy.exe@gmail.com>
2025-03-16 21:19:13 +0400

    Добавлены .sln и Program.cs

---
+++ Patterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using Patterns.HomeWork.HomeWork140325.Manager;
+
+var config1 =  ConfigurationManager.GetInstance();
+var config2 =  ConfigurationManager.GetInstance();
+
+// проверка того, что запускается один экземпляр объекта
+
+Console.Write("Текущий конфиг:");
+config1.ShowConfig();
+Console.WriteLine("-------------");
+Console.WriteLine("Меняем конфиг в config1");
+//записываем настройку в конфиг
+config1.setConfig("Запись в конфиг-1", 1);
+// тот же результат и в config2
+Console.WriteLine("Смотрим, что в config2");
+config2.ShowConfig();
