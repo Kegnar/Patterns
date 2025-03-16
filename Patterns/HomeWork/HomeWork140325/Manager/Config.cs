384afdce617d0cc2240fa1b67a7b2942cd56d2b2
Kegnar <mitriy.exe@gmail.com>
2025-03-16 20:42:24 +0400

    Реализован паттерн Singleton для ConfigurationManager

---
+++ Patterns/HomeWork/HomeWork140325/Manager/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Patterns.HomeWork.HomeWork140325.Manager;
+
+//класс, содержащий некий конфиг нашей программы
+class Config()
+{
+    public string _param1 = "Начальное значение";
+    public int _param2 = 0;
+};
---
+++ Patterns/HomeWork/HomeWork140325/Manager/ConfigurationManager.cs
@@ -0,0 +1,36 @@
+﻿// Вариант 2: Создайте класс ConfigurationManager, который будет использовать паттерн Singleton для
+// управления конфигурацией приложения. Убедитесь, что конфигурация загружается только один
+// раз.
+namespace Patterns.HomeWork.HomeWork140325.Manager;
+
+public class ConfigurationManager 
+{
+  private Config _config = new Config(); //чего-то там в конфиге
+  private static ConfigurationManager instance; // ссылка на объект
+  
+  private ConfigurationManager() 
+  {
+    // тут будет какая-то логика конструктора
+  }
+
+  public void ShowConfig()
+  {
+    Console.WriteLine($"Строковый параметр из настроек: {_config._param1}");
+    Console.WriteLine($"Числовой параметр из настроек: {_config._param2}");
+  }
+
+  public void setConfig(string param1, int param2)
+  {
+    _config._param1 = param1;
+    _config._param2 = param2;
+  }
+
+  public static ConfigurationManager GetInstance()
+  {
+    if (instance == null)
+    {
+      instance = new ConfigurationManager();
+    }
+    return instance;
+  }
+}
